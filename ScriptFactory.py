4b2a8abaed3dffb2a9db0fe26dfcd3c14adc8a95
Dominique Dresen <dominique.dresen@uni-koeln.de>
2017-11-20 21:56:13 +0100

    Cleaned and commented ScriptFactory class

--- ScriptFactory.py
+++ ScriptFactory.py
@@ -1,7 +1,8 @@
-import numpy as np
-import sys, os, datetime, glob, importlib
 from Logger import Logger
 from Experiment import Experiment
+
+import numpy as np
+import sys, os, datetime, glob, importlib
 
 class ScriptFactory():
     def __init__(self):
@@ -14,30 +15,37 @@ class ScriptFactory():
         self.run_factory()
 
     def run_factory(self):
-        self.log_header()
-        self.get_arguments()
-
+        self.log_header() #Initialize header and put into log
+        
+        self.get_arguments() # Get arguments from CL as well as the savepath
+    
+        # Get and check path to folder containing templates
         self.experiment_path = os.path.dirname(os.path.realpath(__file__))+\
                                 '/experiments/'+self.arg_experiment
-        print(self.experiment_path)
         self.check_assertion(os.path.isdir(self.experiment_path),\
                              'Specified experiment folder does not exist.')
 
+        # Generate Experiment object.
+        # If new definition of Experiment is in path use that, otherwise use default
         try:
-            self.experiment = getattr(importlib.import_module('experiments.' +\
-                                  self.arg_experiment + '.experiment'),\
-                                  'Experiment')(self, self.additional_arguments,\
-                                                self.save_template_to)
-            self.log.comment('Using experiment.py from '+self.arg_experiment+' folder to generate script.')
+            self.experiment = getattr(importlib.import_module('experiments.'+\
+                              self.arg_experiment + '.experiment'),\
+                              'Experiment')(self, self.additional_arguments,\
+                                            self.save_template_to)
+            self.log.comment('Using experiment.py from '+self.arg_experiment+\
+                             ' folder to generate script.')
         except ModuleNotFoundError:
             self.experiment = Experiment(self, self.additional_arguments,\
                                          self.save_template_to)
-            self.log.comment('Using default experiment.py to generate file from template folder.')
-        
+            self.log.comment('Using default experiment.py to generate file '+\
+                             'from template folder.')
+    
+        # Read desired template and save to file        
         self.experiment.read_template()
         self.experiment.save_template_to_file()
 
     def log_header(self):
+        # Generate a header included in generated script
         self.log.comment('Initialized ScriptFactory v'+str(self.version))
         self.log.comment('Date: ' + str(datetime.datetime.now()))
 
@@ -47,6 +55,7 @@ class ScriptFactory():
         self.log.log('')
 
     def get_arguments(self):
+        # Get arguments from CL
         self.passed_args = sys.argv
         self.N_passed_args = len(self.passed_args) - 1
         
@@ -56,24 +65,32 @@ class ScriptFactory():
 
         # Experiment folders are always uppercase
         self.arg_experiment = self.passed_args[1].upper()
-        self.log.comment('Preparing Script for Experiment: ' + self.arg_experiment)
+        self.log.comment('Preparing Script for Experiment: ' +\
+                         self.arg_experiment)
         self.additional_arguments = self.passed_args[2:]
         self.N_experiment_args = len(self.additional_arguments)
 
-
-        if self.N_experiment_args >= 2 and self.additional_arguments[-1].endswith('.py'):
+        # If name for saving is given as last argument use this one
+        if self.N_experiment_args >= 2 and\
+                self.additional_arguments[-1].endswith('.py'):
             self.save_template_to = self.additional_arguments[-1]
+        
+        # Otherwise propose one from experiment & template name 
         else:
             self.save_template_to = self.arg_experiment+'_'+\
                                     self.passed_args[2]+'.py'
 
+            # Ask if that is ok. User can otherwise propose his own.
             check_savepath = input('Saving file to ['+self.save_template_to+\
                                    ']. Specify other filename or press ENTER.')
             if not check_savepath.strip() in ['', 'y', 'Y', 'yes', 'Yes','YES',\
                                           'ok', 'j', 'J',]:
+                if not check_savepath.endswith('.py'): #Savefile must be a python file
+                    check_savepath = check_savepath+'.py'
                 self.save_template_to = check_savepath
 
     def check_assertion(self, condition, err_message, child_func=None):
+        # Help method to check if conditions are fulfilled. Otherwise exit with error message
         try:
             assert(condition)
         except:
@@ -85,19 +102,9 @@ class ScriptFactory():
             print(err_message)
             sys.exit()
 
-
     def error_help(self):
-        # print(" #####                                  #######")
-        # print("#     #  ####  #####  # #####  #####    #         ##    ####  #####  ####  #####  #   #")
-        # print("#       #    # #    # # #    #   #      #        #  #  #    #   #   #    # #    #  # #")
-        # print(" #####  #      #    # # #    #   #      #####   #    # #        #   #    # #    #   #")
-        # print("      # #      #####  # #####    #      #       ###### #        #   #    # #####    #")
-        # print("#     # #    # #   #  # #        #      #       #    # #    #   #   #    # #   #    #")
-        # print(" #####   ####  #    # # #        #      #       #    #  ####    #    ####  #    #   #")
-        # print('')
-        print('HELP')
+        # Message which is posted in case of an error
         print('Usage: ScriptFactory.py EXPERIMENT TEMPLATEFILE [additional arguments]')
-
 
 if __name__ == '__main__':
     sf = ScriptFactory()
